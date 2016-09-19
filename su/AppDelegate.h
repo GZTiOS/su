d3a6c4b85b75104196788c82458dccabd66e19e0
GMac <gmac@GMacs-MacBook-Air.local>
2016-09-19 19:54:58 +0800

    Initial Commit

---
+++ su/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  su
+//
+//  Created by GMac on 16/9/19.
+//  Copyright © 2016年 GMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ su/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  su
+//
+//  Created by GMac on 16/9/19.
+//  Copyright © 2016年 GMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
