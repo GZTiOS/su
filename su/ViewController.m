3d2e5cf4b8d13a75041f10902ded0508e83b829a
GZTiOS <gmac@GMacs-MacBook-Air.local>
2016-10-29 18:24:35 +0800

    32
    3444

--- su/ViewController.m
+++ su/ViewController.m
@@ -16,8 +16,42 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+   UIScrollView* scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, 320, 460)];
+    scrollView.backgroundColor = [UIColor redColor];
+    // 是否支持滑动最顶端
+    //    scrollView.scrollsToTop = NO;
+
+    // 设置内容大小
+    scrollView.contentSize = CGSizeMake(320, 460*10);
+    // 是否反弹
+        scrollView.bounces = NO;
+    // 是否分页
+    //    scrollView.pagingEnabled = YES;
+    // 是否滚动
+    //    scrollView.scrollEnabled = NO;
+    //    scrollView.showsHorizontalScrollIndicator = NO;
+    // 设置indicator风格
+        scrollView.indicatorStyle = UIScrollViewIndicatorStyleWhite;
+    // 设置内容的边缘和Indicators边缘
+    //    scrollView.contentInset = UIEdgeInsetsMake(0, 50, 50, 0);
+    //    scrollView.scrollIndicatorInsets = UIEdgeInsetsMake(0, 50, 0, 0);
+    // 提示用户,Indicators flash
+    [scrollView flashScrollIndicators];
+    // 是否同时运动,lock
+    scrollView.directionalLockEnabled = YES;
+    [self.view addSubview:scrollView];
+    
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 200, 320, 40)];
+    label.backgroundColor = [UIColor yellowColor];
+    label.text = @"学习scrolleview";
+    [scrollView addSubview:label];
+
+    NSLog(@"yes");
+    
 }
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
---
+++ su/mvpViewController.h
@@ -0,0 +1,13 @@
+//
+//  mvpViewController.h
+//  su
+//
+//  Created by GMac on 16/10/29.
+//  Copyright © 2016年 GMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mvpViewController : UIViewController
+
+@end
